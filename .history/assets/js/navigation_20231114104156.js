94e9e4474179486e79d9169a8a0544db40867c6e
Trần Anh Toàn <trananhtoan@192.168.2.10>
2023-11-14 12:54:33 +0700

    upload source code

---
+++ .history/assets/js/navigation_20231114104156.js
@@ -0,0 +1,22 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll(".app-fixed-list").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
+console.log(activePage);
+document.addEventListener("DOMContentLoaded", function () {
+  var menuItems = document.querySelectorAll(".app-fixed-list");
+
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Xóa lớp 'active' từ tất cả các mục
+      menuItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      // Thêm lớp 'active' cho mục được nhấp
+      item.classList.add("active");
+    });
+  });
+});
